dce4bc70fdd8c7971a4e92ce08df020d58f992de
MrKleeblatt <mrkleeblatt@gmail.com>
2024-10-23 17:31:55 +0200

    feature: added http server instead of just console logs

--- main.go
+++ main.go
@@ -3,15 +3,43 @@ package main
 import (
 	"almacal/auth"
 	"almacal/calendar"
+	"encoding/base64"
 	"fmt"
-	"os"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
-func main() {
-	sessionno, menuid := auth.Login(os.Args[1], os.Args[2])
-	var ical = calendar.DownloadIcalFile(sessionno, menuid)
-	fmt.Println("ICAL:\n", ical)
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain")
+	w.Header().Add("Content-Disposition", "inline")
+		credentialsArgument := r.URL.Query().Get("credentials")
+	bytes, err := base64.StdEncoding.DecodeString(credentialsArgument)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	credentials := strings.Split(string(bytes), ":")
+	if len(credentials) != 2 {
+		return
+	}
+	username := credentials[0]
+	password := credentials[1]
+	sessionno, menuid := auth.Login(username, password)
+	date := time.Now()
+	var result strings.Builder
+	for range 12 {
+		_, err := result.WriteString(calendar.IcalFile(sessionno, menuid, date.Format("Y2006M01")))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		date.AddDate(0, 1, 0)
+	}
+	fmt.Fprint(w, strings.ReplaceAll(result.String(), "END:VCALENDAR\nBEGIN:VCALENDAR", ""))
+
 }
 
-
-
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("[::1]:8000", nil)
+}
